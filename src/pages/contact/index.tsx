2b67ca1acefd218ed6d87a3573b23059059be2c4
Oleksandr Savelev <a.savelev.krm@gmail.com>
2024-04-20 15:25:39 +0300

    contact page animation

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -10,7 +10,12 @@ const IndexPage = () => (
   <Layout>
     <div className={styles.wrapper}>
       <div className={styles.about}>
-        <div className={styles.about__img_wrapper}>
+        <div
+          data-sal="slide-up"
+          data-sal-delay="100"
+          data-sal-duration="200"
+          data-sal-easing="ease"
+        >
           <StaticImage
             src="../../images/girls_with_bubblegum.png"
             alt="Girl with bubblegum"
@@ -19,7 +24,13 @@ const IndexPage = () => (
           />
         </div>
 
-        <div className={styles.about__info}>
+        <div
+          className={styles.about__info}
+          data-sal="slide-up"
+          data-sal-delay="100"
+          data-sal-duration="300"
+          data-sal-easing="ease"
+        >
           <h3 className={styles.about__heading}>Hello There!</h3>
           <p className={styles.about__paragraph}>
             My name is Elizabeth. I create pixel art portraits, game assets,
@@ -30,13 +41,16 @@ const IndexPage = () => (
           </p>
         </div>
       </div>
-      <div className={styles.contact}>
+      <div
+        className={styles.contact}
+        data-sal="slide-up"
+        data-sal-delay="300"
+        data-sal-duration="400"
+        data-sal-easing="ease"
+      >
         <p className={styles.contact__text}> For any questions:</p>
 
-        <a
-          className={styles.contact__btn}
-          href="mailto:XXXXXXXXXXXXXXXXXXXXXXXXXX"
-        >
+        <a className={styles.contact__btn} href="mailto:elimaximova@gmail.com">
           Contact me
         </a>
       </div>
